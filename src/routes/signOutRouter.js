c8fa192d4087c14c65f0906978c993e483e40153
leo <celsolnv27@gmail.com>
2022-02-10 14:05:29 -0300

    Preparando revisão de código

--- src/routes/signOutRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { signOut } from '../controllers/signOutController.js';
-import { authMiddleware } from '../middlewares/authMiddleware.js';
-
-const signOutRouter = Router();
-
-signOutRouter.delete('/sign-out', authMiddleware, signOut);
-
-export default signOutRouter;
